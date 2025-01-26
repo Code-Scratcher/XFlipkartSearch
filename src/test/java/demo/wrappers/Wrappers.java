e9d3cd9b7964eb330cdcf3fd2b62eb1a16ab6d1b
Debopriyo Dey <debopriyadey97558@gmail.com>
2025-01-26 20:26:33 +0530

    added wrapper methods for findElement and click

--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,22 +1,91 @@
 package demo.wrappers;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.time.Duration;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    // to close initial login popup
+    public static void loginPopUpClose(WebDriver driver) {
+        try {
+            String closeButtonXpath = "//div[@class='JFPqaw']//span[@role='button']";
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            if (wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(closeButtonXpath))) != null) {
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath(closeButtonXpath))).click();    
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Error closing popup : " + e.getMessage());
+        }
+    }
+
+    public static WebElement findWebElement(WebDriver driver, By locator, int retryCount, long retryDelay) throws InterruptedException {
+        int attempt = 0;
+        while (attempt<retryCount) {
+            try {
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+                wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(locator));
+                WebElement webElements = driver.findElement(locator);
+                return webElements;
+            } catch (Exception e) {
+                attempt++;
+                if(attempt<retryCount) {
+                    System.out.println("Attempt " + attempt + " failed to find element. Retrying in " + retryDelay + " seconds.");
+                } else {
+                    System.out.println("Error finding element after " + retryCount + " attempts: " + e.getMessage());
+                    throw e;
+                }
+                Thread.sleep(retryDelay * 1000);
+                System.out.println("Error finding element : " + e.getMessage());
+                throw e;
+            }
+        }
+        return null;
+    }
+
+    public static List<WebElement> findWebElementList(WebDriver driver, By locator, int retryCount, long retryDelay) throws InterruptedException {
+        int attempt = 0;
+        while (attempt<retryCount) {
+            try {
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+                wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(locator));
+                List<WebElement> webElements = driver.findElements(locator);
+                return webElements;
+            } catch (Exception e) {
+                attempt++;
+                if(attempt<retryCount) {
+                    System.out.println("Attempt " + attempt + " failed to find element. Retrying in " + retryDelay + " seconds.");
+                } else {
+                    System.out.println("Error finding element after " + retryCount + " attempts: " + e.getMessage());
+                    throw e;
+                }
+                Thread.sleep(retryDelay * 1000);
+                System.out.println("Error finding element : " + e.getMessage());
+                throw e;
+            }
+        }
+        return null;
+    }
+
+    public static void clickWebElement(WebDriver driver, By locator) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+            wait.until(ExpectedConditions.and(ExpectedConditions.visibilityOfElementLocated(locator),
+            ExpectedConditions.elementToBeClickable(locator)));
+            driver.findElement(locator).click();
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Error in clicking element : "+e.getMessage());
+        }
+    }
+
 }
